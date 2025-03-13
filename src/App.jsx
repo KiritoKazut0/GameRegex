a2bc8c8479b99f4ccc45145e0978e3b79893177c
KiritoKazut0 <norbertolopez291@gmail.com>
2025-03-12 23:35:49 -0600

    implements router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import LoginForm from "./pages/Login"
 import HomePage from "./pages/Home"
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+
+
+const myRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginForm/>
+  },
+
+  {
+    path: '/home',
+    element: <HomePage/>
+  }
+])
 
 function App() {
-
-
-  return (
-    <>
-       {/* <LoginForm></LoginForm>  */}
-       <HomePage></HomePage>  
-    
-    </>
-  )
+  return <RouterProvider router={myRouter}/>
 }
 
 export default App
